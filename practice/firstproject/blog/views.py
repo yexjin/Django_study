1c6924ee820b654c760ed5ed49002804aea4c633
yexjin <tnska0421@naver.com>
2021-06-30 12:09:44 +0900

    [6]CRUD_complete

--- practice/firstproject/blog/views.py
+++ practice/firstproject/blog/views.py
@@ -1,12 +1,42 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
+from django.utils import timezone
 from .models import Blog
 
-# Create your views here.
 def blog(request):
-    # Blog 테이블에 있는 객체들을 싹 다 가져와서 blogs에 저장
     blogs = Blog.objects.all()
     return render(request,'blog.html',{'blogs':blogs})
 
 def detail(request, id):
     blog = get_object_or_404(Blog,pk = id)
     return render(request, 'detail.html', {'blog':blog})
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    new_blog = Blog()
+    new_blog.title = request.POST['title']
+    new_blog.writer = request.POST['writer']
+    new_blog.body = request.POST['body']
+    new_blog.pub_date = timezone.now( )    
+    new_blog.save() 
+    return redirect('detail', new_blog.id)
+
+def edit(request, id):
+    edit_blog = Blog.objects.get(id= id)
+    return render(request, 'edit.html', {'blog':edit_blog})
+
+def update(request, id):
+    update_blog = Blog.objects.get(id= id)
+    update_blog.title = request.POST['title']
+    update_blog.writer = request.POST['writer']
+    update_blog.body = request.POST['body']
+    update_blog.pub_date = timezone.now( )    
+    update_blog.save() 
+    return redirect('detail', update_blog.id)
+
+# DELETE !!
+def delete(request, id):
+    delete_blog = Blog.objects.get(id=id)
+    delete_blog.delete()
+    return redirect('blog')
--- practice/firstproject/firstproject/urls.py
+++ practice/firstproject/firstproject/urls.py
@@ -1,18 +1,16 @@
 from django.contrib import admin
 from django.urls import path
-# views로 겹칠 경우 에러가 발생해서 as로 각각 이름지어주기
-# from firstapp import views as first
-# from wordCount import views as wc
-from blog.views import * # 모든함수를 가져오겠다.
+from blog.views import * 
 from blog import views as blog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('welcome', first.welcome, name="welcome"),
-    # name이라는 세번째 인수는 다른 html에서 url대신에 쉽게 사용할 수 있는 이름이다.
-    # path('hello', first.hello, name="hello"),
-    # path('wc/', wc.home, name="wc"),
-    # path('wc/result/', wc.result, name="result"),
     path('', blog.blog, name="blog"),
     path('<str:id>', detail, name="detail"),
+    path('new/',new,name="new"),
+    path('create/',create, name="create"),
+    path('edit/<str:id>', edit, name="edit"),
+    path('update/<str:id>', update, name="update"),
+    ## delete!!
+    path('delete/<str:id>', delete, name="delete"),
 ]
